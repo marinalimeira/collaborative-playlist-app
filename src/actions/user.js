8e54d25c33ef8658cff7eb16ecd7be9101725f7a
Marina Limeira <marinaflessa@gmail.com>
2016-12-12 04:35:31 -0300

    Get user info after login

--- src/actions/user.js
+++ src/actions/user.js
@@ -9,6 +9,19 @@ export const userRegisterRequest = (data) => (
 export const userLoginRequest = (data) => (
   dispatch => (
     $.post('http://localhost:3000/oauth/token', { ...data, grant_type: "password" })
-        .done((data) => localStorage.setItem('token', data.access_token))
+        .done((response) => {
+          const token = response.access_token;
+          localStorage.setItem('token', token);
+          $.ajax({
+            method: 'GET',
+            url: 'http://localhost:3000/api/v1/users/me',
+            beforeSend: function (request) {
+                request.setRequestHeader('Authorization', `Bearer ${token}`)
+            },
+          }).done((data) => {
+            localStorage.setItem('name', data.name)
+            localStorage.setItem('email', data.email)
+          })
+        })
   )
 )
